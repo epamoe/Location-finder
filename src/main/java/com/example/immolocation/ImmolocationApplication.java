86c50872ed63c4bef9e08b746f9111382a56488e
Tchougwen01 <tewobrice@gmail.com>
2022-05-03 23:36:15 +0100

    immolocation:structure

---
+++ src/main/java/com/example/immolocation/ImmolocationApplication.java
@@ -0,0 +1,14 @@
+package com.example.immolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmolocationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmolocationApplication.class, args);
+        System.out.print("bonjour");
+    }
+
+}
---
+++ src/test/java/com/example/immolocation/ImmolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.immolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmolocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
