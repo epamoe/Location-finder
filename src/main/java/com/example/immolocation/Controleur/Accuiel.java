69299edf7f39195bbde8ede1857072ec818a56e1
Tchougwen01 <tewobrice@gmail.com>
2022-05-09 15:58:01 +0100

    mapping de la page d'accueil

---
+++ src/main/java/com/example/immolocation/Controleur/Accuiel.java
@@ -0,0 +1,15 @@
+package com.example.immolocation.Controleur;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Accuiel {
+
+    @GetMapping(value="/home")
+    public String accueil()
+    {
+
+        return "Accueil";
+    }
+}
--- src/main/java/com/example/immolocation/Controleur/Bailleur_controlleur.java
+++ src/main/java/com/example/immolocation/Controleur/Bailleur_controlleur.java
@@ -4,18 +4,10 @@ import com.example.immolocation.Dao.BailleurRepository;
 import com.example.immolocation.Model.Bailleur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Bailleur_controlleur {
-    @Autowired
-    BailleurRepository repoBailleur;
-
-    @RequestMapping("/addBailleur")
-
-    public String ajouter_Bailleur( Bailleur bailleur){
-        repoBailleur.save(bailleur);
-        return "auth_bailleur";
-    }
 
 }
