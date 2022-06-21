58401ce468dd6bc1c812246a7626994491bec923
Algor103 <junioralgor@gmail.com>
2022-06-21 05:45:10 +0000

    Mise a jour MessageController.java

--- src/main/java/com/example/immolocation/Controleur/MessageController.java
+++ src/main/java/com/example/immolocation/Controleur/MessageController.java
@@ -4,21 +4,25 @@ import com.example.immolocation.Model.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MessageController {
-  /*  private SimpMessagingTemplate simpMessagingTemplate;
+    private SimpMessagingTemplate simpMessagingTemplate;
     @MessageMapping("/message")
     @SendTo("/chatroom/public")
-    private Message recevoir_message_public(@Payload Message message){
-          return message;
+    public Message sendMessage(@Payload Message message) {
+        return message;
     }
 
-    @MessageMapping("/message-prive")
-    public Message recevoir_message_prive(@Payload Message message) {
-        simpMessagingTemplate.convertAndSendToUser(message.getNom_recepteur(), "/private", message);
+    @MessageMapping("/message.addUser")
+    @SendTo("/chatroom/public")
+    public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+      // ajout d'un nom d'utilisateur dans une session de web socket
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
         return message;
-    }*/
+    }
+
 }
